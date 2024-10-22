19a34323cbef6eafb97c75eebbc380f2d97ff4f1
Vid Tadel <tadelv@gmail.com>
2024-10-22 22:20:38 +0200

    wip combined graph

--- lib/ui/widgets/shot_graph.dart
+++ lib/ui/widgets/shot_graph.dart
@@ -64,24 +64,26 @@ class ShotGraphState extends State<ShotGraph> {
       child: Column(
         children: [
           ...shotOverlay.map(
-            (e) => Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text(
-                  '${DateFormat.Hm().format(e!.date)} ${DateFormat.yMd().format(e.date)} ${e.pourWeight.toStringAsFixed(1)}g in ${e.pourTime.toStringAsFixed(1)}s'),
-              TextButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ShotEdit(
-                                e.id,
-                              )),
-                    );
-                  },
-                  icon: const Icon(Icons.note_add),
-                  label: Text(S.of(context).screenEspressoDiary))
-            ]),
+            (e) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                      '${DateFormat.Hm().format(e!.date)} ${DateFormat.yMd().format(e.date)} ${e.pourWeight.toStringAsFixed(1)}g in ${e.pourTime.toStringAsFixed(1)}s'),
+                  TextButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ShotEdit(
+                                    e.id,
+                                  )),
+                        );
+                      },
+                      icon: const Icon(Icons.note_add),
+                      label: Text(S.of(context).screenEspressoDiary))
+                ]),
           ),
-          _buildGraphs()['single'],
+          _buildGraphs()['combined'],
         ],
       ),
     );
@@ -98,14 +100,17 @@ class ShotGraphState extends State<ShotGraph> {
     }
 
     var single = _buildGraphSingleFlCharts(datamap, ranges);
-    return {"single": single};
+    var combined = _builGraphCombinedFlCharts(datamap, ranges);
+    return {"single": single, "combined": combined};
   }
 
   Iterable<VerticalRangeAnnotation> _createPhasesFl(List<ShotState> states) {
-    var stateChanges = states.where((element) => element.subState.isNotEmpty).toList();
+    var stateChanges =
+        states.where((element) => element.subState.isNotEmpty).toList();
 
     int i = 0;
-    var maxSampleTime = states.isNotEmpty ? states.last.sampleTimeCorrected : 10.0;
+    var maxSampleTime =
+        states.isNotEmpty ? states.last.sampleTimeCorrected : 10.0;
     return stateChanges.map((from) {
       var toSampleTime = maxSampleTime;
 
@@ -125,22 +130,44 @@ class ShotGraphState extends State<ShotGraph> {
     });
   }
 
-  Map<String, List<FlSpot>> _createDataFlCharts(int id, List<ShotState> shotstates) {
+  Map<String, List<FlSpot>> _createDataFlCharts(
+      int id, List<ShotState> shotstates) {
     return {
-      "pressure$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.groupPressure)).toList(),
-      "pressureSet$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.setGroupPressure)).toList(),
-      "flow$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.groupFlow)).toList(),
-      "flowSet$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.setGroupFlow)).toList(),
-      "temp$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.headTemp)).toList(),
-      "tempSet$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.setHeadTemp)).toList(),
-      "tempMix$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.mixTemp)).toList(),
-      "tempMixSet$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.setMixTemp)).toList(),
-      "weight$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.weight)).toList(),
-      "flowG$id": shotstates.map((e) => FlSpot(e.sampleTimeCorrected, e.flowWeight)).toList(),
+      "pressure$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.groupPressure))
+          .toList(),
+      "pressureSet$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.setGroupPressure))
+          .toList(),
+      "flow$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.groupFlow))
+          .toList(),
+      "flowSet$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.setGroupFlow))
+          .toList(),
+      "temp$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.headTemp))
+          .toList(),
+      "tempSet$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.setHeadTemp))
+          .toList(),
+      "tempMix$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.mixTemp))
+          .toList(),
+      "tempMixSet$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.setMixTemp))
+          .toList(),
+      "weight$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.weight))
+          .toList(),
+      "flowG$id": shotstates
+          .map((e) => FlSpot(e.sampleTimeCorrected, e.flowWeight))
+          .toList(),
     };
   }
 
-  LineChartBarData createChartLineDatapoints(List<FlSpot> points, double barWidth, Color col, List<int>? dash) {
+  LineChartBarData createChartLineDatapoints(
+      List<FlSpot> points, double barWidth, Color col, List<int>? dash) {
     return LineChartBarData(
       spots: points,
       dotData: const FlDotData(
@@ -153,15 +180,121 @@ class ShotGraphState extends State<ShotGraph> {
     );
   }
 
-  Widget _buildGraphSingleFlCharts(Map<String, List<FlSpot>> data, Iterable<VerticalRangeAnnotation> ranges) {
+  Widget _builGraphCombinedFlCharts(Map<String, List<FlSpot>> data,
+      Iterable<VerticalRangeAnnotation> ranges) {
+    List<LineChartBarData> lineBarsData = [];
+    double i = 0;
+    for (var id in overlayIds!) {
+      lineBarsData.add(createChartLineDatapoints(data["pressure$id"]!, 4,
+          calcColor(theme.ThemeColors.pressureColor, i), null));
+      lineBarsData.add(createChartLineDatapoints(
+          data["pressureSet$id"]!,
+          2,
+          calcColor(
+            theme.ThemeColors.pressureColor,
+            i,
+          ),
+          [5, 5]));
+      lineBarsData.add(createChartLineDatapoints(data["flow$id"]!, 4,
+          calcColor(theme.ThemeColors.flowColor, i), null));
+      lineBarsData.add(createChartLineDatapoints(data["flowSet$id"]!, 2,
+          calcColor(theme.ThemeColors.flowColor, i), [5, 5]));
+      lineBarsData.add(createChartLineDatapoints(data["flowG$id"]!, 2,
+          calcColor(theme.ThemeColors.weightColor, i), null));
+      lineBarsData.add(createChartLineDatapoints(data["weight$id"]!, 2,
+          calcColor(theme.ThemeColors.weightColor, i), null));
+      lineBarsData.add(createChartLineDatapoints(data["temp$id"]!, 4,
+          calcColor(theme.ThemeColors.tempColor, i), null));
+      lineBarsData.add(createChartLineDatapoints(data["tempSet$id"]!, 2,
+          calcColor(theme.ThemeColors.tempColor, i), [5, 5]));
+      lineBarsData.add(createChartLineDatapoints(data["tempMix$id"]!, 4,
+          calcColor(theme.ThemeColors.tempColor2, i), null));
+      lineBarsData.add(createChartLineDatapoints(data["tempMixSet$id"]!, 2,
+          calcColor(theme.ThemeColors.tempColor2, i), [5, 5]));
+      i += 0.25;
+    }
+    var flowChart1 = LineChart(
+      LineChartData(
+        // minY: 0,
+        // maxY: 15,
+        // minX: data["pressure${overlayIds!.first}"]!.first.x,
+        // maxX: maxTime,
+        lineTouchData: const LineTouchData(enabled: false),
+        clipData: const FlClipData.all(),
+        gridData: const FlGridData(
+          show: true,
+          drawVerticalLine: true,
+        ),
+        lineBarsData: lineBarsData,
+        rangeAnnotations: RangeAnnotations(
+          verticalRangeAnnotations: [
+            ...ranges,
+          ],
+        ),
+        titlesData: FlTitlesData(
+          topTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          rightTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          // ),
+          bottomTitles: !widget.showTemp && !widget.showWeight
+              ? AxisTitles(
+                  axisNameSize: 25,
+                  axisNameWidget: Text(
+                    S.of(context).graphTime,
+                    style: Theme.of(context).textTheme.labelSmall,
+                    // style: TextStyle(
+                    //     // fontSize: 15,
+                    //     ),
+                  ),
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: bottomTitleWidgets,
+                    reservedSize: 26,
+                  ),
+                )
+              : AxisTitles(
+                  axisNameSize: 25,
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                    getTitlesWidget: bottomTitleWidgets,
+                    reservedSize: 26,
+                  ),
+                ),
+          show: true,
+          leftTitles: AxisTitles(
+            axisNameSize: 25,
+            axisNameWidget: Text(
+              S.of(context).graphFlowMlsPressureBar,
+              style: Theme.of(context).textTheme.labelSmall,
+            ),
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: leftTitleWidgets,
+              reservedSize: 56,
+            ),
+          ),
+        ),
+      ),
+    );
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: SizedBox(height: 300, child: flowChart1)
+    );
+  }
+
+  Widget _buildGraphSingleFlCharts(Map<String, List<FlSpot>> data,
+      Iterable<VerticalRangeAnnotation> ranges) {
     List<LineChartBarData> lineBarsDataFlows = [];
     List<LineChartBarData> lineBarsDataTempWeight = [];
     List<LineChartBarData> lineBarsDataTemp = [];
     double i = 0;
     for (var id in overlayIds!) {
       if (widget.showPressure) {
-        lineBarsDataFlows.add(
-            createChartLineDatapoints(data["pressure$id"]!, 4, calcColor(theme.ThemeColors.pressureColor, i), null));
+        lineBarsDataFlows.add(createChartLineDatapoints(data["pressure$id"]!, 4,
+            calcColor(theme.ThemeColors.pressureColor, i), null));
         lineBarsDataFlows.add(createChartLineDatapoints(
             data["pressureSet$id"]!,
             2,
@@ -172,26 +305,26 @@ class ShotGraphState extends State<ShotGraph> {
             [5, 5]));
       }
       if (widget.showFlow) {
-        lineBarsDataFlows
-            .add(createChartLineDatapoints(data["flow$id"]!, 4, calcColor(theme.ThemeColors.flowColor, i), null));
-        lineBarsDataFlows
-            .add(createChartLineDatapoints(data["flowSet$id"]!, 2, calcColor(theme.ThemeColors.flowColor, i), [5, 5]));
-        lineBarsDataFlows
-            .add(createChartLineDatapoints(data["flowG$id"]!, 2, calcColor(theme.ThemeColors.weightColor, i), null));
+        lineBarsDataFlows.add(createChartLineDatapoints(data["flow$id"]!, 4,
+            calcColor(theme.ThemeColors.flowColor, i), null));
+        lineBarsDataFlows.add(createChartLineDatapoints(data["flowSet$id"]!, 2,
+            calcColor(theme.ThemeColors.flowColor, i), [5, 5]));
+        lineBarsDataFlows.add(createChartLineDatapoints(data["flowG$id"]!, 2,
+            calcColor(theme.ThemeColors.weightColor, i), null));
       }
       if (widget.showWeight) {
-        lineBarsDataTempWeight
-            .add(createChartLineDatapoints(data["weight$id"]!, 2, calcColor(theme.ThemeColors.weightColor, i), null));
+        lineBarsDataTempWeight.add(createChartLineDatapoints(data["weight$id"]!,
+            2, calcColor(theme.ThemeColors.weightColor, i), null));
       }
       if (widget.showTemp) {
-        lineBarsDataTemp
-            .add(createChartLineDatapoints(data["temp$id"]!, 4, calcColor(theme.ThemeColors.tempColor, i), null));
-        lineBarsDataTemp
-            .add(createChartLineDatapoints(data["tempSet$id"]!, 2, calcColor(theme.ThemeColors.tempColor, i), [5, 5]));
-        lineBarsDataTemp
-            .add(createChartLineDatapoints(data["tempMix$id"]!, 4, calcColor(theme.ThemeColors.tempColor2, i), null));
-        lineBarsDataTemp.add(
-            createChartLineDatapoints(data["tempMixSet$id"]!, 2, calcColor(theme.ThemeColors.tempColor2, i), [5, 5]));
+        lineBarsDataTemp.add(createChartLineDatapoints(data["temp$id"]!, 4,
+            calcColor(theme.ThemeColors.tempColor, i), null));
+        lineBarsDataTemp.add(createChartLineDatapoints(data["tempSet$id"]!, 2,
+            calcColor(theme.ThemeColors.tempColor, i), [5, 5]));
+        lineBarsDataTemp.add(createChartLineDatapoints(data["tempMix$id"]!, 4,
+            calcColor(theme.ThemeColors.tempColor2, i), null));
+        lineBarsDataTemp.add(createChartLineDatapoints(data["tempMixSet$id"]!,
+            2, calcColor(theme.ThemeColors.tempColor2, i), [5, 5]));
       }
       i += 0.25;
     }
@@ -419,16 +552,24 @@ class ShotGraphState extends State<ShotGraph> {
           //   ),
           // ),
           if (widget.showFlow || widget.showPressure)
-            SizedBox(height: height - (widget.showTemp ? 120 : 0) - (widget.showWeight ? 120 : 0), child: flowChart1),
+            SizedBox(
+                height: height -
+                    (widget.showTemp ? 120 : 0) -
+                    (widget.showWeight ? 120 : 0),
+                child: flowChart1),
           if (widget.showWeight) const SizedBox(height: 20),
           if (widget.showWeight)
             SizedBox(
-                height: height - (widget.showFlow || widget.showPressure ? 300 : 0) - (widget.showTemp ? 120 : 0),
+                height: height -
+                    (widget.showFlow || widget.showPressure ? 300 : 0) -
+                    (widget.showTemp ? 120 : 0),
                 child: flowChart2),
           if (widget.showTemp) const SizedBox(height: 20),
           if (widget.showTemp)
             SizedBox(
-                height: height - (widget.showFlow || widget.showPressure ? 300 : 0) - (widget.showWeight ? 120 : 0),
+                height: height -
+                    (widget.showFlow || widget.showPressure ? 300 : 0) -
+                    (widget.showWeight ? 120 : 0),
                 child: flowChart3),
         ],
       ),
