9e5bacabf258a0f2b4d2787151b0a125cd1ec8c1
Vid Tadel <tadelv@gmail.com>
2024-11-19 20:08:43 +0100

    simplify meater reading

--- lib/devices/meater_thermometer.dart
+++ lib/devices/meater_thermometer.dart
@@ -16,11 +16,14 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
   // ignore: non_constant_identifier_names
   Uuid ServiceUUID = Uuid.parse('a75cc7fc-c956-488f-ac2a-2dbc08b63a04');
   // ignore: non_constant_identifier_names
-  static Uuid CharateristicUUID = Uuid.parse('7edda774-045e-4bbf-909b-45d1991a2876');
+  static Uuid CharateristicUUID =
+      Uuid.parse('7edda774-045e-4bbf-909b-45d1991a2876');
   // ignore: non_constant_identifier_names
-  static Uuid BatteryServiceUUID = Uuid.parse('a75cc7fc-c956-488f-ac2a-2dbc08b63a04');
+  static Uuid BatteryServiceUUID =
+      Uuid.parse('a75cc7fc-c956-488f-ac2a-2dbc08b63a04');
   // ignore: non_constant_identifier_names
-  static Uuid BatteryCharacteristicUUID = Uuid.parse('2adb4877-68d8-4884-bd3c-d83853bf27b8');
+  static Uuid BatteryCharacteristicUUID =
+      Uuid.parse('2adb4877-68d8-4884-bd3c-d83853bf27b8');
 
   late TempService tempService;
 
@@ -35,7 +38,8 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
   MeaterThermometer(this.device, this.connection) {
     tempService = getIt<TempService>();
     log.info("Connect to Meater ${device.serviceUuids}");
-    _deviceListener = connection.connectToDevice(id: device.id).listen((connectionState) {
+    _deviceListener =
+        connection.connectToDevice(id: device.id).listen((connectionState) {
       _onStateChange(connectionState.connectionState);
     }, onError: (Object error) {
       // Handle a possible error
@@ -51,20 +55,9 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
   }
 
   void _notificationCallback(List<int> pData) {
-    int tip = bytesToInt(pData[0], pData[1]);
-    int ra = bytesToInt(pData[2], pData[3]);
-    int oa = bytesToInt(pData[4], pData[5]);
-    double ambient2 = tip + (max(0.0, ((((ra - min(48, oa)) * 16) * 589)) / 1487));
+    int meat_raw = ((pData[1] << 8) + (pData[0]));
 
-    // int meat_raw = ((pData[1] << 8) + (pData[0]));
-    // double temp_int = (meat_raw).toDouble() / 16;
-    // int ambient_raw = ((pData[3] << 8) + (pData[2]));
-    // int offset = ((pData[5] << 8) + (pData[4]));
-    // double ambient = (meat_raw + max(0, (ambient_raw - offset)) * 6.33) / 16;
-    // log.info(
-    //     "Data $meat_raw, TempInt: $temp_int, Ambient Raw:$ambient_raw, $offset, Ambient:$ambient 2:${toCelsius(ambient2.toInt())} TIP:${toCelsius(tip)}");
-
-    tempService.setTemp(toCelsius(tip), toCelsius(ambient2.toInt()));
+    tempService.setTemp(toCelsius(meat_raw), 0);
   }
 
   void _onStateChange(DeviceConnectionState state) async {
@@ -80,10 +73,13 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
         log.info('Connected');
         tempService.setState(TempState.connected);
         // await device.discoverAllServicesAndCharacteristics();
-        final characteristic =
-            QualifiedCharacteristic(serviceId: ServiceUUID, characteristicId: CharateristicUUID, deviceId: device.id);
+        final characteristic = QualifiedCharacteristic(
+            serviceId: ServiceUUID,
+            characteristicId: CharateristicUUID,
+            deviceId: device.id);
 
-        _characteristicsSubscription = connection.subscribeToCharacteristic(characteristic).listen((data) {
+        _characteristicsSubscription =
+            connection.subscribeToCharacteristic(characteristic).listen((data) {
           // code to handle incoming data
           _notificationCallback(data);
         }, onError: (dynamic error) {
@@ -92,9 +88,12 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
 
         try {
           final batteryCharacteristic = QualifiedCharacteristic(
-              characteristicId: BatteryCharacteristicUUID, serviceId: BatteryServiceUUID, deviceId: device.id);
+              characteristicId: BatteryCharacteristicUUID,
+              serviceId: BatteryServiceUUID,
+              deviceId: device.id);
 
-          connection.subscribeToCharacteristic(batteryCharacteristic).listen((data) {
+          connection.subscribeToCharacteristic(batteryCharacteristic).listen(
+              (data) {
             // code to handle incoming data
             int bat = bytesToInt(data[0], data[1]) * 10;
             tempService.setBattery(bat);
@@ -102,7 +101,8 @@ class MeaterThermometer extends ChangeNotifier implements AbstractThermometer {
             log.severe("Error subscribing to battery characteristics $error");
           });
 
-          final batteryLevel = await connection.readCharacteristic(batteryCharacteristic);
+          final batteryLevel =
+              await connection.readCharacteristic(batteryCharacteristic);
           int bat = bytesToInt(batteryLevel[0], batteryLevel[1]) * 10;
           tempService.setBattery(bat);
         } catch (e) {
